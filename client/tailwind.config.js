8460030beb8f46169c94f332ee39535701d66860
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-19 00:03:36 +0000

    slightly better

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],  theme: {
+  content: ["./src/index.{html,js}"],
+  theme: {
     extend: {},
   },
   plugins: [],
